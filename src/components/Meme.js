3c3c91cf8c4208a7dc7aedcb8f45a9ae3747ef02
Anthony Pageau <antopageau@hotmail.com>
2023-11-01 14:27:31 -0400

    using api instead of json file

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,4 @@
 import React from "react"
-import memesData from "../memesData.js"
-import "../style.css"
 
 export default function Meme() {
 
@@ -9,24 +7,17 @@ export default function Meme() {
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
-
-    function handleChange(event) {
-      const {name, value} = event.target
-      setMeme(prevMeme => {
-        return {
-          ...prevMeme,
-          [name]: value
-        }
-      })
-    }
+    const [allMemes, setAllMemes] = React.useState([])
     
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
     
-    function getMemeImage(event) {
-        event.preventDefault()
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
@@ -34,39 +25,45 @@ export default function Meme() {
         
     }
     
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    
     return (
         <main>
-          <form>
             <div className="form">
-                  <input 
-                      type="text"
-                      placeholder="Top text"
-                      onChange={handleChange}
-                      name="topText"
-                      value={meme.topText}
-                      className="form--input"
-                  />
-                  <input 
-                      type="text"
-                      placeholder="Bottom text"
-                      onChange={handleChange}
-                      name="bottomText"
-                      value={meme.bottomText}
-                      className="form--input"
-                  />
-                  <button 
-                      className="form--button"
-                      onClick={getMemeImage}
-                  >
-                      Get a new meme image 🖼
-                  </button>
-              </div>
-              <div className="meme">
-                  <img src={meme.randomImage} className="meme--image" />
-                  <h2 className="meme--text top">{meme.topText}</h2>
-                  <h2 className="meme--text bottom">{meme.bottomText}</h2>
-              </div>
-          </form>
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
